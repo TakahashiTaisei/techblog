b9a41ef7a8c8f8830a4cdfa6ee40dbcd760abcfe
TakahahshiTaisei <takahashi@cvlab.cs.tsukuba.ac.jp>
2020-06-27 01:40:12 +0900

    ZOZOの記事リンクを読み込み

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
 import requests
-import bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
-url = "https://techblog.zozo.com"
+url = "https://techblog.zozo.com/archive/2020"
 
 r = requests.get(url)
 
 soup = BeautifulSoup(r.text, 'html.parser')
 
-elems = soup.find_all("a", class_="reference internal")
+"""
+for aa in soup.find_all("a"):
+            link = aa.get("href")
+            name = aa.get_text()
+            print(link,"\t",name)
+"""
+
+
+
+elems = soup.find_all("a", class_="entry-title-link")
 for e in elems:
-  print(e.getText())
+  print(e.getText(),e.get("href"))
